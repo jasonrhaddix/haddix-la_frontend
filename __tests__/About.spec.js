090bc8519d393e7c99cc62785200ecc64afd4aa6
Jason R. Haddix <jasonrhaddix@gmail.com>
2025-05-17 17:42:19 -0700

    Removed test

--- __tests__/About.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import AboutView from '@/views/AboutView.vue'
-
-describe('AboutView.vue', () => {
-  test('renders correctly and matches snapshot', () => {
-    const wrapper = shallowMount(AboutView)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
--- __tests__/App.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import App from '@/App.vue'
-
-describe('App.vue', () => {
-  test('renders correctly and matches snapshot', () => {
-    const wrapper = shallowMount(App)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
