1e21aea45cc91dc713676a1fe1a7b3d095f6a5af
Jason R. Haddix <jasonrhaddix@gmail.com>
2025-05-17 17:02:08 -0700

    N8N message updates

--- __tests__/Animated_Logo.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import AnimatedLogoContainer from './AnimatedLogoContainer.vue'
-
-describe('AnimatedLogoContainer.vue', () => {
-  test('renders correctly and matches snapshot', () => {
-    const wrapper = shallowMount(AnimatedLogoContainer)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
--- __tests__/App_Button.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import AppButton from './AppButton.vue';
-
-describe('AppButton.vue', () => {
-  test('matches snapshot', () => {
-    const wrapper = shallowMount(AppButton);
-    expect(wrapper.html()).toMatchSnapshot();
-  });
-});
