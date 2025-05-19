0e3abefbc65664b038ecb5a794faeb1a24cf4816
Jason R. Haddix <jasonrhaddix@gmail.com>
2025-05-19 00:09:52 -0700

    Removed test

--- __tests__/Navigation.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import NavigationContainer from '@/components/NavigationContainer.vue'
-
-describe('NavigationContainer.vue', () => {
-  test('renders correctly', () => {
-    const wrapper = shallowMount(NavigationContainer)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
